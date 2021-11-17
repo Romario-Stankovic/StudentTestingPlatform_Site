0aac550dfd16be49ce5cbb8f61bd044d118383d6
Romario <38624716+Romario-Stankovic@users.noreply.github.com>
2021-11-17 23:02:49 +0100

    Added Professor login page Optimized layout of fields in css Added a new function for login

--- js/login.js
+++ js/login.js
@@ -1,5 +1,12 @@
 function studentLogin(e){
-    let indexField = document.getElementById("indexField");
+    let indexNo = document.getElementById("indexField").value;
     e.preventDefault();
-    console.log(indexField.value);
+    console.log(indexNo);
+}
+
+function professorLogin(e){
+    let username = document.getElementById("usernameField").value;
+    let password = document.getElementById("passwordField").value;
+    e.preventDefault();
+    console.log(username + " - " + password);
 }
