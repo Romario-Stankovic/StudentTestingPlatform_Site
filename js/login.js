7900b5f966968b14ac249b8851c94592a30495c7
Romario <38624716+Romario-Stankovic@users.noreply.github.com>
2022-01-21 19:20:54 +0100

    Initial API Implementation Removed old scripts

--- js/login.js
+++ js/login.js
@@ -1,25 +1,44 @@
+let errorMessage;
+
+function showErrorMessage(message){
+    if(message == null){
+        errorMessage.style.display = "none";
+    }else{
+        errorMessage.style.display = "block";
+        errorMessage.innerText = message;
+    }
+}
+
 function studentLogin(event){
 
     event.preventDefault();
 
     let indexNumber = document.getElementById("indexField").value;
-    getStudent(indexNumber);
-}
-
-function getStudent(index){
     $.ajax({
         url: "http://localhost:4000/auth/login/student",
         method: "POST",
-        dataType: "json",
         data: {
-            indexNumber: index
+            indexNumber: indexNumber
         },
-        success: function(data){
-            console.log(data);
+        success: function(data) {
+                if(data.code == undefined){
+                    localStorage.setItem("token", data.token);
+                    localStorage.setItem("refreshToken", data.refreshToken);
+                    window.location = "studentTests.html";
+                }else if(data.code == 4001){
+                    showErrorMessage("Invalid index number!");
+                }
+        },
+        statusCode:
+        {
+            400: function(){
+                showErrorMessage("Index number is in the wrong format");
+            }
         }
-    })
+    });
 }
 
-$(document).ready(() => {
-    
+$(function() {
+    errorMessage = document.getElementById("loginError");
+    showErrorMessage(null);
 });
